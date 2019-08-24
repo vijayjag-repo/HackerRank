94c0747fbbe74b45daf089691ead6395db90e222
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2019-08-23 20:29:10 -0500

    Solution

--- HR_Sock_Merchant.py
+++ HR_Sock_Merchant.py
@@ -8,20 +8,18 @@ import sys
 
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
-    count = 0
     d = dict()
-    for i in range(len(ar)):
-        if ar[i] not in d:
-            d[ar[i]] = 1
+    count = 0
+    for item in ar:
+        if(item in d):
+            d[item]+=1
         else:
-            d[ar[i]]+=1
+            d[item] = 1
 
-    for i in d:
-        while(d[i]>=2):
-            d[i]-=2
-            count+=1
+    for val in d.values():
+        count+=val//2
     return(count)
-    
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -34,3 +32,4 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
